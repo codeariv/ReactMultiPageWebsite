78454654e698fe70f459bd4e1909a35031f903d4
Syamlal CM <syamjayaraj@gmail.com>
2021-01-15 09:07:23 +0530

    minor

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,23 +4,12 @@ import { Link, withRouter } from "react-router-dom";
 function Navigation(props) {
   return (
     <div className="navigation">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
         <div class="container">
           <Link class="navbar-brand" to="/">
             React Multi-Page Website
           </Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
+          <div>
             <ul class="navbar-nav ml-auto">
               <li
                 class={`nav-item  ${
