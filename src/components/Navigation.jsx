65b33dd0ef8dba17be8481775a1a185bf507c994
totok30 <91183674+totok30@users.noreply.github.com>
2021-09-22 15:49:31 +0700

    initial commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,41 +4,15 @@ import { Link, withRouter } from "react-router-dom";
 function Navigation(props) {
   return (
     <div className="navigation">
-      <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <nav class="navbar navbarf-expand navbar-light bg-light">
         <div class="container">
-          <Link class="navbar-brand" to="/">
-            React Multi-Page Website
-          </Link>
           <div>
             <ul class="navbar-nav ml-auto">
               <li
                 class={`nav-item  ${
                   props.location.pathname === "/" ? "active" : ""
                 }`}
-              >
-                <Link class="nav-link" to="/">
-                  Home
-                  <span class="sr-only">(current)</span>
-                </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/about" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/contact" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </li>
+              ></li>
             </ul>
           </div>
         </div>
