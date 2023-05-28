0824105d9d443a70cf2f4567779639f59fa67f93
Dave Melkonian <davemelk@gmail.com>
2023-05-28 19:45:59 -0400

    home page scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
       <img className="img-art-two" src={recordtwo} alt="artwork" />
       <div className="container">
         <div className="row my-5">
-          <div className="col-lg-12 mx-3">
+          <div className="col-lg-12 mx-0">
             <div className="row">
               <div className="col-lg-12 mb-3">
                 <Person />
