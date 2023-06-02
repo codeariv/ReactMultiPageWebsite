e6e24205ae98e3e88ceae74ae9f074a08e36d912
Dave Melkonian <davemelk@gmail.com>
2023-06-02 10:33:38 -0400

    updates to verbiage and added more skills

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -49,6 +49,49 @@ function Home() {
                 </p>
               </div>
 
+              <div className="row my-5 mx-1 d-flex flex-row">
+              <div className="col-lg-12 p-4">
+                <h3>Here are the concepts, software, tooling and methodologies I've used or facilitated</h3>
+    
+    <p>
+    Typescript
+Javascript
+ReactJs
+Angular
+Razor
+jQuery
+UX/XD/CX/IXD/UI
+Peer Leadership
+Team Leadership
+Management
+SPAs
+Micro Frontends
+Microservices
+REST
+API
+AJAX
+Github
+VersionOne
+Subversion
+Jira
+Confluence
+.NET
+Contextual Inquiry
+Task flows
+User flows
+Empathy maps
+User research
+Usability Testing
+Heuristic Reviews
+Technical consulting
+    </p>
+  </div>
+</div>
+
+
+
+
+
 
 
 
